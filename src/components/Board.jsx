a4dc39b3dbe1dc40bbc4691e68ae582e963f32a9
John Pieri <jvpieri@gmail.com>
2024-02-28 23:20:02 -0400

    Added more styling and initial props

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,10 +1,13 @@
 import List from "./List.jsx"
+import kanbanData from "../data.json"
 
 function Board() {
+    console.log(kanbanData)
   return (
     <section className="board-container">
-        Board
-        <List/>
+        <List type="To Do"/>
+        <List type="In Progress"/>
+        <List type="Done"/>
     </section>
   )
 }
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,10 +1,15 @@
 import Card from "./Card.jsx"
 
-function List() {
+function List(props) {
+
   return (
-    <div>
-    List
-    <Card/>    
+    <div className="list-columns">
+        <span className="title-type-box">
+            <h1>{props.type}</h1>   
+        </span>
+        <div className="card-container">
+            <Card/>
+        </div>
     </div>
   )
 }
