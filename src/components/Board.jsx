8d69b4c38affe56f19f7ad1710cdccf4bf36885d
John Pieri <jvpieri@gmail.com>
2024-03-03 16:46:43 -0400

    Added list components and lifted state up to Board.

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,13 +1,22 @@
 import List from "./List.jsx"
+
+import { useState } from "react"
+
 import kanbanData from "../data.json"
 
 const Board = () => {
 
+  const [cards, setCards] = useState(kanbanData)
+  
   return (
     <section className="board-container">
-        <List type="To Do"  kanbanData={kanbanData}/>
+        {/* <List type="To Do"  kanbanData={kanbanData}/>
         <List type="In Progress"  kanbanData={kanbanData}/>
-        <List type="Done"  kanbanData={kanbanData}/>
+        <List type="Done"  kanbanData={kanbanData}/> */}
+
+        <List type="To Do" cards={cards} setCards={setCards}/>
+        <List type="In Progress" cards={cards} setCards={setCards}/>
+        <List type="Done" cards={cards} setCards={setCards}/>
     </section>
   )
 }
