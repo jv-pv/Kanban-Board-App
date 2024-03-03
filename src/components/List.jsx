f45bf8fa8d6e348aad3e55a5d48f124d6b042d4b
John Pieri <jvpieri@gmail.com>
2024-03-03 16:47:21 -0400

    Added logig for editing current cards in list

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,16 +1,52 @@
 import Card from "./Card.jsx"
+
 import { useState } from "react"
 
-const List = ({type, kanbanData}) => {
+const List = ({type, cards, setCards}) => {
 
-  const [cards, setCards] = useState(kanbanData)
+  // const [newTask, setNewTask] = useState({
+  //   id: "",
+  //   title: "",
+  //   "description": "Investigate and fix the login problem reported by users.",
+  //   "assignee": "Mark Johnson",
+  //   "status": "In Progress",
+  //   "priority": "High",
+  //   "createdDate": "2023-09-17",
+  //   "dueDate": "2023-09-25"
+  // })
 
+  // const handleTextChange = (e) => {
+  //   setNewTask((prev) => ({...prev, ["id"]: randomIdGenerator()}))
+  //   setNewTask((prev) => ({...prev, [e.target.name]: e.target.value})) 
+  // }
+
+  const handleTaskChange = (id, updatedTask) => {
+    let thisIndex;
+    let thisCard = cards.find((card, i) => {
+      thisIndex = i;
+      return card.id === id;
+    })
+
+    console.log("This is the task we're updating", thisCard, thisIndex)
+
+    let newCards = [...cards];
+    newCards[thisIndex] = updatedTask;
+    setCards(newCards);
+
+  };
+
+  // const addTask = (newTask) => {
+  //   let newTasks = [newTask, ...cards]
+  //   setCards(newTasks)
+  // }
+
+  
   let filteredByType = cards.filter(data => {
     return data.status === type
   })
 
   const handleDelete = (cardId) => {
-    let filteredCards = filteredByType.filter(card => {
+    let filteredCards = cards.filter(card => {
       return card.id !== cardId
     })
     setCards(filteredCards)
@@ -21,6 +57,7 @@ const List = ({type, kanbanData}) => {
         <Card
         key={data.id}
         onDelete={handleDelete}
+        handleTaskChange={handleTaskChange}
         {...data}
         />
     )
@@ -34,6 +71,9 @@ const List = ({type, kanbanData}) => {
         </span>
         <div className="card-container">
             {cardComponents}
+        </div>
+        <div className="new-task-container">
+          <button>Add New Task</button>
         </div>
     </div>
   )
