f75b47203759416858e430c48743ddc4f2ca113c
John Pieri <jvpieri@gmail.com>
2024-03-03 16:48:17 -0400

    Added nav links for page navigation with react router dom

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,14 +2,14 @@ import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
 
-
-
   return (
   <aside className="sidebar-container">
     <ul>
-      <li><NavLink to="/" >Home</NavLink></li>
-      <li><NavLink to="/board" >Board</NavLink></li>
-      <li><NavLink to="/about" >About</NavLink></li>
+
+      <li><NavLink to="/" className={({isActive}) => isActive ? "selected" : ""} >Home</NavLink></li>
+      <li><NavLink to="/board" className={({isActive}) => isActive ? "selected" : ""}>Board</NavLink></li>
+      <li><NavLink to="/about" className={({isActive}) => isActive ? "selected" : ""}>About</NavLink></li>
+      
     </ul>
   </aside>
   )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,24 @@
 import Sidebar from "../components/Sidebar"
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
 
   return (
     <div className="main-content-container">
         <Sidebar/>
+        <div className="home-content-container">
+            <h1>Welcome to React Kanban!</h1>
+
+          <div className="current-boards-wrapper">
+            <h2>Current Boards:</h2>
+                <div className="boards-card">
+                    <h3>Board 1</h3>
+                    <Link to="/board" className="board-card-link">View Board</Link>
+                </div>
+          </div>
+
+        </div>
+
     </div>
   )
 }
